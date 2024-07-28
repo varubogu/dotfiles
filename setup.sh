a2f2ef28e5f03cd8259ab016817a8e29ad3d6133
varubogu <toyosuke@varubogu.com>
2024-07-29 05:09:55 +0900

    関数命名を修正

--- setup.sh
+++ setup.sh
@@ -3,39 +3,25 @@
 DOTFILES_REPO_URL="https://github.com/varubogu/dotfiles.git"
 DOTFILES_BRANCH="main"
 
-if_git_installed() {
-    if command -v git &> /dev/null; then
+is_command_exists() {
+    if command -v $1 &> /dev/null; then
         return 0
     else
         return 1
     fi
 }
 
-if_brew_installed() {
-    if command -v brew &> /dev/null; then
-        return 0
-    else
-        return 1
-    fi
-}
 
-if_apt_installed() {
-    if command -v apt-get &> /dev/null; then
-        return 0
-    else
-        return 1
-    fi
-}
 
-checking_git() {
+setup_git() {
     echo "Checking git..."
-    if if_git_installed; then
+    if is_command_exists git; then
         echo "git already installed"
     else
         echo "git is not installed. Installing git..."
-        if if_brew_installed; then
+        if is_brew_installed; then
             brew install git
-        elif if_apt_installed; then
+        elif is_apt_installed; then
             sudo apt-get update
             sudo apt-get install -y git
         else
@@ -45,23 +31,22 @@ checking_git() {
     fi
 }
 
-yadm_ready() {
+setup_yadm() {
     echo "Checking yadm..."
     if command -v yadm &> /dev/null; then
         echo "yadm already installed"
     else
         echo "yadm is not installed. Installing yadm..."
-        if if_brew_installed; then
+        if is_command_exists brew; then
             brew install yadm
-        elif if_apt_installed; then
+        elif is_command_exists apt-get; then
             sudo apt-get update && sudo apt-get install -y yadm
         else
             echo "Unable to install yadm. Please install it manually."
             exit 1
         fi
     fi
-}
-yadm_latest() {
+
     echo "Cloning dotfiles..."
     if [ -d ~/.local/share/yadm/repo.git ]; then
         echo "yadm is already initialized"
@@ -72,17 +57,17 @@ yadm_latest() {
     fi
 }
 
+
 main() {
-    checking_git
-    yadm_ready
-    yadm_latest
+    setup_git
+    setup_yadm
 
     # 各シェルに実行権限付与
     find ~/dotfiles -name "*.sh" -exec chmod +x {} \;
 
     echo "Installed dotfiles successfully!"
     echo "Please run the following command"
-    echo "`echo $SHELL` ~/dotfiles/setup_after.sh"
+    echo "`$SHELL` ~/dotfiles/setup_after.sh"
 }
 
 main
