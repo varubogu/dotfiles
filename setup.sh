13907e737423598808fcd656e1c334dbbd8768f9
varubogu <toyosuke@varubogu.com>
2024-07-28 21:20:15 +0900

    インストール方法をyadmを使うように修正

--- setup.sh
+++ setup.sh
@@ -1,29 +1,88 @@
 #!/bin/bash
 
-if command -v git &> /dev/null; then
-    echo "git already installed"
-else
-    echo "git is not installed. Installing git..."
-    if command -v brew &> /dev/null; then
-        brew install git
-    elif command -v apt-get &> /dev/null; then
-        sudo apt-get update
-        sudo apt-get install -y git
+DOTFILES_REPO_URL="https://github.com/varubogu/dotfiles.git"
+DOTFILES_BRANCH="main"
+
+if_git_installed() {
+    if command -v git &> /dev/null; then
+        return 0
     else
-        echo "Unable to install git. Please install it manually."
-        exit 1
+        return 1
     fi
-fi
+}
 
-if [ ! -d ~/dotfiles ]; then
-    echo "dotfiles is not found"
-    git clone https://github.com/varubogu/dotfiles.git ~/dotfiles
-else
-    echo "dotfiles is found"
-    git -C ~/dotfiles pull
-fi
+if_brew_installed() {
+    if command -v brew &> /dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
 
-echo "Starting installation..."
+if_apt_installed() {
+    if command -v apt-get &> /dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
 
-# 各シェルに実行権限付与
-find ~/dotfiles -name "*.sh" -exec chmod +x {} \;
+checking_git() {
+    echo "Checking git..."
+    if if_git_installed; then
+        echo "git already installed"
+    else
+        echo "git is not installed. Installing git..."
+        if if_brew_installed; then
+            brew install git
+        elif if_apt_installed; then
+            sudo apt-get update
+            sudo apt-get install -y git
+        else
+            echo "Unable to install git. Please install it manually."
+            exit 1
+        fi
+    fi
+}
+
+yadm_ready() {
+    echo "Checking yadm..."
+    if command -v yadm &> /dev/null; then
+        echo "yadm already installed"
+    else
+        echo "yadm is not installed. Installing yadm..."
+        if if_brew_installed; then
+            brew install yadm
+        elif if_apt_installed; then
+            sudo apt-get update && sudo apt-get install -y yadm
+        else
+            echo "Unable to install yadm. Please install it manually."
+            exit 1
+        fi
+    fi
+}
+yadm_latest() {
+    echo "Cloning dotfiles..."
+    if [ -d ~/.local/share/yadm/repo.git ]; then
+        echo "yadm is already initialized"
+        yadm pull origin $DOTFILES_BRANCH
+    else
+        echo "yadm is none repository. Cloning dotfiles..."
+        yadm clone $DOTFILES_REPO_URL
+    fi
+}
+
+main() {
+    checking_git
+    yadm_ready
+    yadm_latest
+
+    # 各シェルに実行権限付与
+    find ~/dotfiles -name "*.sh" -exec chmod +x {} \;
+
+    echo "Installed dotfiles successfully!"
+    echo "Please run the following command"
+    echo "`echo $SHELL` ~/dotfiles/setup_after.sh"
+}
+
+main
