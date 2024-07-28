ec1d39f63a47ed0f156b0fb3f54054119d28fc08
varubogu <toyosuke@varubogu.com>
2024-07-28 19:38:44 +0900

    GPG鍵ダウンロード時、yesを自動的に選択

--- bin/init/install_apt.sh
+++ bin/init/install_apt.sh
@@ -19,7 +19,7 @@ sudo apt-get install build-essential procps file -y
 echo "installing 1password..."
 sudo -s \
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+gpg --yes --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
 tee /etc/apt/sources.list.d/1password.list
 mkdir -p /etc/debsig/policies/AC2D62742012EA22/
@@ -27,12 +27,12 @@ curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
 tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
 mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+gpg --yes --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 apt update && apt install 1password 1password-cli -y
 
 echo "installing docker ..."
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
