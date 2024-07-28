f2b676ba2563bac86b62636e00d187a4ebd2a81e
varubogu <toyosuke@varubogu.com>
2024-07-28 19:39:05 +0900

    環境変数定義の関数名を変更

--- bin/init/xdg_base_dir.sh
+++ bin/init/xdg_base_dir.sh
@@ -1,4 +1,4 @@
-def_env() {
+safe_env() {
     local var_name=$1
     local value=$2
 
@@ -10,21 +10,21 @@ def_env() {
 }
 
 # XDG Base Directory Specification
-def_env "XDG_RUNTIME_DIR" "/run/user/$(id -u)"
-def_env "XDG_CONFIG_DIRS" "/etc/xdg"
-def_env "XDG_DATA_DIRS" "/usr/local/share:/usr/share"
-def_env "XDG_CONFIG_HOME" "$HOME/.config"
-def_env "XDG_CACHE_HOME" "$HOME/.cache"
-def_env "XDG_DATA_HOME" "$HOME/.local/share"
-def_env "XDG_STATE_HOME" "$HOME/.local/state"
-def_env "XDG_AUTOSTART_DIR" "$XDG_CONFIG_HOME/autostart"
+safe_env "XDG_RUNTIME_DIR" "/run/user/$(id -u)"
+safe_env "XDG_CONFIG_DIRS" "/etc/xdg"
+safe_env "XDG_DATA_DIRS" "/usr/local/share:/usr/share"
+safe_env "XDG_CONFIG_HOME" "$HOME/.config"
+safe_env "XDG_CACHE_HOME" "$HOME/.cache"
+safe_env "XDG_DATA_HOME" "$HOME/.local/share"
+safe_env "XDG_STATE_HOME" "$HOME/.local/state"
+safe_env "XDG_AUTOSTART_DIR" "$XDG_CONFIG_HOME/autostart"
 
 # XDG user directories
-def_env "XDG_DESKTOP_DIR" "$HOME/Desktop"
-def_env "XDG_DOCUMENTS_DIR" "$HOME/Documents"
-def_env "XDG_DOWNLOAD_DIR" "$HOME/Downloads"
-def_env "XDG_MUSIC_DIR" "$HOME/Music"
-def_env "XDG_PICTURES_DIR" "$HOME/Pictures"
-def_env "XDG_PUBLICSHARE_DIR" "$HOME/Public"
-def_env "XDG_TEMPLATES_DIR" "$HOME/Templates"
-def_env "XDG_VIDEOS_DIR" "$HOME/Videos"
+safe_env "XDG_DESKTOP_DIR" "$HOME/Desktop"
+safe_env "XDG_DOCUMENTS_DIR" "$HOME/Documents"
+safe_env "XDG_DOWNLOAD_DIR" "$HOME/Downloads"
+safe_env "XDG_MUSIC_DIR" "$HOME/Music"
+safe_env "XDG_PICTURES_DIR" "$HOME/Pictures"
+safe_env "XDG_PUBLICSHARE_DIR" "$HOME/Public"
+safe_env "XDG_TEMPLATES_DIR" "$HOME/Templates"
+safe_env "XDG_VIDEOS_DIR" "$HOME/Videos"
