122b00cff9da3f43f7022c9420caa9484d2514db
varubogu <toyosuke@varubogu.com>
2024-07-29 08:53:03 +0900

    editorconfigのシンボリックリンクを作成

--- .local/bin/init/symlink.sh
+++ .local/bin/init/symlink.sh
@@ -111,6 +111,7 @@ safe_symlink $DOTCONFIG/bash/.bash_profile $HOME/.bash_profile
 safe_symlink $DOTCONFIG/zsh/.zshrc $HOME/.zshrc
 safe_symlink $DOTCONFIG/zsh/.zprofile $HOME/.zprofile
 safe_symlink $DOTCONFIG/zsh/.zshrc.lazy $HOME/.zshrc.lazy
+safe_symlink $DOTCONFIG/editorconfig/.editorconfig $HOME/.editorconfig
 
 
 safe_copy $DOTCONFIG/git/config_local $HOME/.gitconfig
