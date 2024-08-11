c7ff9322770d0ae62dbf0425bf814be97c97c9cc
varubogu <toyosuke@varubogu.com>
2024-08-12 08:42:31 +0900

    関数定義ミスを修正

--- .local/bin/init/install.sh
+++ .local/bin/init/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+. $HOME/dotfiles/.local/bin/lib/env_os.sh
 . $HOME/dotfiles/.local/bin/lib/command.sh
 
 echo "install os specific packages"
 
-if is_macos; then
+if is_mac; then
     echo "apt is available on this system"
     ./install_mac.sh
 fi
