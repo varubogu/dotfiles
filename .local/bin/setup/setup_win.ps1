f0b75a3ba793104d31758886d7e89a2a5323976d
varubogu <toyosuke@varubogu.com>
2024-11-11 19:57:47 +0900

    構成変更（動作未確認）

---
+++ .local/bin/setup/setup_win.ps1
@@ -0,0 +1,54 @@
+# PowerShell スクリプト
+
+# エラー発生時に実行を停止
+$ErrorActionPreference = "Stop"
+
+$REPO_OWNER = "varubogu"
+$REPO_NAME = "dotfiles"
+$REPO_URL = "https://github.com/$REPO_OWNER/$REPO_NAME.git"
+$REPO_RAW = "https://raw.github.com/$REPO_OWNER/$REPO_NAME"
+$BRANCH = "main"
+$BIN_DIR = "$HOME/$REPO_NAME/.local/bin"
+
+function Setup-Yadm {
+    Write-Host "Checking yadm..."
+    if (Get-installedScript -name yadm) {
+        Write-Host "yadm already installed"
+    }
+    else {
+        Write-Host "yadm is not installed. Installing yadm..."
+        Install-Script -Name yadm -Scope CurrentUser
+    }
+
+    Write-Host "Cloning dotfiles.to yadm..."
+    if (Test-Path "$HOME/.local/share/yadm/repo.git") {
+        Write-Host "yadm is already initialized"
+        yadm pull origin $BRANCH
+    }
+    else {
+        Write-Host "yadm is none repository. Cloning dotfiles..."
+        yadm clone $REPO_URL
+    }
+}
+
+function Main {
+    # ホームディレクトリに移動
+    Set-Location ~
+
+    # yadmをインストール & dotfilesをclone
+    Setup-Yadm
+
+    # XDG Base Directory Specificationを設定
+    Start-Process pwsh.exe -ArgumentList "-File `"$BIN_DIR\xdg_base_dir\setEnv.ps1`"" -Verb RunAs
+
+    # wingetでアプリを一括インストール
+    winget import -i .\.config\winget\20241110_w2022.json
+
+    # シンボリックリンクを貼る
+    Write-Host "symlink execution"
+    . "$BIN_DIR/symlink/symlink.ps1"
+
+    Write-Host "Installed dotfiles successfully!"
+}
+
+Main
--- .local/bin/xdg_base_dir/setEnv.ps1
+++ .local/bin/xdg_base_dir/setEnv.ps1
@@ -1,6 +1,10 @@
 # 管理者権限の確認
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Error "管理者権限が必要です。スクリプトを管理者として実行してください。"
+
+    Write-Host "続行するには何かキーを押してください..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
     exit
 }
 
