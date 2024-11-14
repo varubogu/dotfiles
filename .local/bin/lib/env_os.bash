d1acad4791caae5204d7639089a4499bad568efd
varubogu <toyosuke@varubogu.com>
2024-11-14 20:42:58 +0900

    WSLの判定条件を修正

--- .local/bin/lib/env_os.bash
+++ .local/bin/lib/env_os.bash
@@ -4,20 +4,25 @@ is_linux() { [ "$(uname)" = "Linux" ]; }
 
 is_mac() { [ "$(uname)" = "Darwin" ];}
 
-is_wsl() { grep -qEi "(Microsoft|WSL)" /proc/version; }
+is_wsl() {
+    [ -n "${WSL_DISTRO_NAME}" ] || \
+    [ -n "${WSLG_DIR}" ] || \
+    (grep -qEi "(Microsoft|WSL)" /proc/version 2>/dev/null && \
+    [ -r "/mnt/c/Users" ])
+}
 
 is_windows() {
-  case "$(uname -r)" in
-    *Microsoft*)
-      return 0
-      ;;
-    *CYGWIN*|*MINGW*|*MSYS*)
-      return 0
-      ;;
-    *)
-      return 1
-      ;;
-  esac
+    case "$(uname -r)" in
+        *Microsoft*)
+            return 0
+            ;;
+        *CYGWIN*|*MINGW*|*MSYS*)
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
+    esac
 }
 
 
