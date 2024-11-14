1124872701536082c82a7d27af099fdfce4d663f
varubogu <toyosuke@varubogu.com>
2024-11-14 20:43:50 +0900

    wslとbrewのエイリアス追加

--- .config/alias.zsh
+++ .config/alias.zsh
@@ -2,13 +2,21 @@
 
 BIN_DIR=$HOME/.local/bin
 
+. $BIN_DIR/lib/command.bash
 . $BIN_DIR/lib/env_os.bash
 
-# if is_wsl; then
-#     # SSH by Host(Windows)
-#     alias ssh='ssh.exe'
-#     alias ssh-add='ssh-add.exe'
-# fi
+if is_wsl; then
+    . $BIN_DIR/lib/env_wsl.bash
+    # SSH by Host(Windows)
+    alias ssh='ssh.exe'
+    alias ssh-add='ssh-add.exe'
+fi
+
+# Homebrewのエイリアスと設定
+if is_command_available brew; then
+    alias brewup='brew update && brew upgrade'
+    export PATH="/opt/homebrew/bin:$PATH"
+fi
 
 # パッケージのアップデートコマンド
 alias pup=$BIN_DIR/mng/pkg_update.bash
--- .local/bin/install/install_apt-get.bash
+++ .local/bin/install/install_apt-get.bash
@@ -17,31 +17,28 @@ sudo update-locale LANG=ja_JP.UTF8
 echo "installing tools..."
 sudo apt-get install git zsh curl wget ca-certificates gnupg lsb-release -y
 
-if [ "$is_root" = "true" ]; then
-    if ! is_command_available brew; then
-        echo "installing HomeBrew..."
-        sudo apt-get install build-essential procps file -y
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    fi
+if ! is_command_available brew && [ "$is_root" = "true" ]; then
+    echo "installing HomeBrew..."
+    sudo apt-get install build-essential procps file -y
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if [ "$is_root" = "true" ]; then
-    if ! is_command_available 1password; then
-        echo "installing 1password..."
-        curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-        echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
-        sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-        curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-        sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-        curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-        sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-        curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-        sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-        sudo apt update && sudo apt install 1password 1password-cli -y
-    fi
+
+if ! is_command_available 1password && [ "$is_root" = "true" ]; then
+    echo "installing 1password..."
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+    echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    sudo apt update && sudo apt install 1password 1password-cli -y
 fi
 
-if ! is_command_available docker; then
+if ! is_command_available docker && [ "$is_root" = "true" ]; then
     echo "installing docker ..."
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -52,13 +49,16 @@ if ! is_command_available docker; then
     sudo apt-get install docker-compose-plugin -y
 fi
 
-if [ ! -d "$XDG_CONFIG_HOME/zsh/ohmyzsh" ] && [ ! -d "$HOME/.oh-my-zsh" ]; then
+ohmyzsh_root="$HOME/.oh-my-zsh"
+ohmyzsh_xdg="$XDG_CONFIG_HOME/zsh/ohmyzsh"
+
+if [ ! -d "$ohmyzsh_xdg" ] && [ ! -d "$ohmyzsh_root" ]; then
     echo "installing oh-my-zsh ..."
     yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-    if [ ! -d "$XDG_CONFIG_HOME/zsh/ohmyzsh" ]; then
+    if [ ! -d "$ohmyzsh_xdg" ]; then
         echo "oh-my-zsh already installed"
-    elif [ ! -d "$HOME/.oh-my-zsh" ]; then
+    elif [ ! -d "$ohmyzsh_root" ]; then
         echo "oh-my-zsh already installed"
     fi
 fi
